2d2e9831f73ec2f116257fc6aa7f8b83bf9b5f58
Ho <aho@email.neit.edu>
2024-01-16 13:51:27 -0500

    W2D1 demo done on 1/16

---
+++ w2/demo/secondDemo.py
@@ -0,0 +1,46 @@
+import csv
+
+#initialize counting vars - counts from file
+total_records = 0
+total_salaries = 0
+
+#initialize empty lists - 1 list per FIELD
+names = []
+ages = []
+salaries = []
+
+#connect to the file
+with open("w2/demo/example.csv") as unicorn:
+
+    #read the file - access
+    narwhal = csv.reader(unicorn)
+
+    #go in and "read" each recordin the file
+    for rec in narwhal:
+        #for"for every record found in the file(entire row of field data)"
+
+        #dislay the data VALUES in NEAT columns
+        print(f"{rec[0]:10} \t{rec[1]:3} \t${rec[2]:10}")
+
+        #store data from rec list(current record being read) to list 
+        #adding to a list ----> .append() requires LIST NAME as starting object
+        names.append(rec[0]) #NAME
+        ages.append(int(rec[1])) #AGES
+        salaries.append(float(rec[2])) #SALARY
+
+
+        #keep literal count of number of records
+        total_records += 1
+
+        #keep running total of salaries
+        total_salaries += float(rec[2])
+
+#final total displays
+print(f"TOTAL RECORDS: {total_records} | TOTAL SALARY: ${total_salaries:.2f}")
+
+#process the lists to display the text file information
+#PROCESS LIST --> FOR LOOP!
+#for something in something else
+for index in range(0, total_records):
+    #"for each value in the range of 0 to total number of values in total records"
+    print(f"INDEX: {index} \t {names[index]} is {ages[index]} years old")
