5f0210fab7a7628abfc188a8a2dc4f37a3853019
AndyHo04 <aho@email.neit.edu>
2024-01-23 11:25:57 -0500

    testing done on 1/23

--- w2/demo/secondDemo.py
+++ w2/demo/secondDemo.py
@@ -23,7 +23,7 @@ with open("w2/demo/example.csv") as unicorn:
         print(f"{rec[0]:10} \t{rec[1]:3} \t${rec[2]:10}")
 
         #store data from rec list(current record being read) to list 
-        #adding to a list ----> .append() requires LIST NAME as starting object
+        #adding to a list ----> .append() which requires LIST NAME as starting object
         names.append(rec[0]) #NAME
         ages.append(int(rec[1])) #AGES
         salaries.append(float(rec[2])) #SALARY
@@ -44,3 +44,20 @@ print(f"TOTAL RECORDS: {total_records} | TOTAL SALARY: ${total_salaries:.2f}")
 for index in range(0, total_records):
     #"for each value in the range of 0 to total number of values in total records"
     print(f"INDEX: {index} \t {names[index]} is {ages[index]} years old")
+
+#process through the list to create a total age value
+total_age = 0 
+for index in range(0, total_records):
+    #add each age to a total age variable
+    total_age += ages[index]
+
+#determine the average age. then display
+average_age = total_age / total_records
+print(f"AVERAGE AGE:{average_age:.2f}")
+
+#what it looks like if we do a while loop(comparison to for loop)
+total_age = 0
+index = 0 
+while index < total_records:
+    index += 1
+    total_age += ages[index]
